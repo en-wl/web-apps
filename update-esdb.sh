2f95149b2a76d46243f15df93f51c262c6efddd0
Kevin Atkinson <k@kevina.org>
2026-05-17 23:02:23 -0400

    Checkout from 'deploy' branch when updating.
    Also refactor some of the code into update-esdb.sh meant to be run
    locally for testing.

---
+++ update-esdb.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -ex
+
+GIT_UPDATE="git merge --ff-only"
+
+if [ "$1" = "--force" ]; then
+  GIT_UPDATE="git reset --hard"
+fi
+
+cd scowl
+git clean -q -f -x -d
+git fetch origin v2
+$GIT_UPDATE origin/v2
+make scowl.db
+cd ..
+
